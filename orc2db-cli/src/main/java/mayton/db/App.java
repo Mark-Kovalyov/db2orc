87acfa8570a1ded11413b4c50d164754de0bd134
mayton <mark.kovalyov78@gmail.com>
2020-08-03 22:56:07 +0300

    Add modules

---
+++ orc2db-cli/src/main/java/mayton/db/App.java
@@ -0,0 +1,13 @@
+package mayton.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
